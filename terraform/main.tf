3e1f6c3adf7329b2dfaf888410cb1532d26a03c4
Naveen Guleria <mavenishere@gmail.com>
2022-04-02 22:29:25 +0530

    ml-job update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,17 @@
-resource "aws_sagemaker_code_repository" "example" {
+resource "aws_sagemaker_code_repository" "ml-job" {
   code_repository_name = "ml-source-code"
 
   git_config {
-    repository_url = "https://github.com/tanujjain/deploy-ml-model.git"
+    repository_url = "https://github.com/i-engy/ml-job-temp.git"
   }
 }
 
 
 resource "aws_sagemaker_notebook_instance" "default" {
-  name          = var.sagemaker_notebook_name
-  role_arn      = var.aws_iam_role
-  instance_type = "ml.t2.medium"
+  name                    = var.sagemaker_notebook_name
+  instance_type           = "ml.t2.medium"
+  platform_identifier     = "notebook-al2-v1"
+  volume_size             = 10
+  role_arn                = "arn:aws:iam::909172569716:role/sm_notebook_role"
+  default_code_repository = aws_sagemaker_code_repository.ml-job.code_repository_name
 }
